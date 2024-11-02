94146e98e0025e768782028eefacc2e1546f6ada
Humbledrum <phermes2002@gmail.com>
2024-11-02 02:20:22 +0100

    Self Play random starting position added will be better for training later on

--- src/self_play.py
+++ src/self_play.py
@@ -8,11 +8,14 @@ import tensorflow as tf
 import random
 import json
 import os
+import chess
+
 
 class SelfPlay:
-    def __init__(self, model, num_games=100):
+    def __init__(self, model, num_games=100, random_start_probability=0.5):
         self.model = model
         self.num_games = num_games
+        self.random_start_probability = random_start_probability
         self.data = []  # Stores game states, moves, and outcomes
         self.save_folder = "../self_play_records"
 
@@ -22,11 +25,17 @@ class SelfPlay:
     def play(self):
         for game_index in range(self.num_games):
             print(f"\n--- Starting game {game_index + 1} ---")
-            chess_board = Chessboard()
+
+            if random.random() < self.random_start_probability:
+                random_fen = self.get_random_position()
+                chess_board = Chessboard(starting_position_FEN=random_fen)
+                print(f"Game {game_index + 1} starting from random position: {random_fen}")
+            else:
+                chess_board = Chessboard()
+
             white_agent = Agent(self.model)
             black_agent = Agent(self.model)
             game = Game(chess_board, white_agent, black_agent)
-
             game_data = []
 
             while not chess_board.board.is_game_over():
@@ -57,6 +66,18 @@ class SelfPlay:
         # Saving data of all games as well for now. Will see if this is better for training
         self.save_data(filename=f"{self.save_folder}/self_play_data_all_games.json")
 
+    def get_random_position(self):
+        board = chess.Board()
+        # 2 to 15ish moves for midgame
+        for _ in range(random.randint(2, 15)):
+            if board.is_game_over():
+                board = chess.Board()
+            else:
+                move = random.choice(list(board.legal_moves))
+                board.push(move)
+
+        return board.fen()
+
     def get_game_outcome(self, chess_board):
         result = chess_board.board.result()
         if result == "1-0":
@@ -81,5 +102,5 @@ if __name__ == '__main__':
 
     # Run self-play games
     num_games = 10
-    self_play = SelfPlay(model, num_games)
+    self_play = SelfPlay(model, num_games, 1.0)
     self_play.play()
